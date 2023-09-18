d4e91911efdbdc9d8487989bfa5ea3013c0f4579
zrvnarmarin <76marin76@gmail.com>
2023-09-18 20:42:10 +0200

    joinCommunity and footer sections finished

--- components/community/CommunitySection.tsx
+++ components/community/CommunitySection.tsx
@@ -3,17 +3,17 @@ import { Button } from "../ui/button";
 
 export default function CommunitySection() {
     return (
-        <section className="w-full flex flex-col items-center gap-4 py-32 bg-[#E5C643]">
-            <h2 className=" flex flex-col gap-6 font-black text-4xl text-white uppercase">
+        <section className="w-full flex flex-col items-center gap-4 py-8 px-2 md:py-32 bg-[#E5C643]">
+            <h2 className="text-start sm:text-center gap-6 font-black text-3xl md:text-4xl text-white uppercase">
                 Join shopping community to get monthly promo
             </h2>
-            <p className="font-normal  text-2xl text-white">
+            <p className="font-normal text-start sm:text-center text-xl md:text-3xl text-white">
                 Type your email down below and be young wild generation
             </p>
             <div className="flex items-center justify-center w-fit bg-white rounded-lg p-2 my-6">
                 <Input 
                     placeholder="Add your email here" 
-                    className="px-8 text-lg py-4 outline-none border-none focus:outline-none" 
+                    className="px-8 text-sm md:text-lg py-4 outline-none border-none focus:outline-none" 
                 />
                 <Button 
                     variant={"default"} 
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,15 +3,15 @@ import { FacebookIcon, LinkedinIcon, InstagramIcon, TwitterIcon } from '@/public
 
 export default function Footer() {
     return (
-        <footer className="w-full flex flex-row py-24 px-16  bg-black">
+        <footer className="w-full flex flex-col lg:flex-row lg:gap-16 py-8 md:py-24 px-2 md:px-16 bg-black">
             <div className='flex flex-col gap-8'>
-                <h2 className=" flex flex-col gap-6 font-black text-4xl text-white uppercase">
+                <h2 className="text-center lg:text-start gap-6 font-black text-4xl text-white uppercase">
                     Fashion
                 </h2>
-                <p className="text-[#8E8E8E] text-lg">
+                <p className="text-[#8E8E8E] text-lg text-center lg:text-start">
                     Complete your style with awesome clothes from us.
                 </p>
-                <div className='flex flex-row gap-3'>
+                <div className='flex flex-row justify-center lg:justify-start gap-3'>
                     {socials.map(social => 
                         <Link href={social.href} 
                             className='h-12 w-18 bg-[#EBD96B] flex items-center  py-2 px-4 rounded-2xl' 
@@ -22,9 +22,9 @@ export default function Footer() {
                     )}
                 </div>
             </div>
-            <div className='w-full flex justify-end items-start gap-32'>
+            <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-12 lg:mt-0 md:gap-2'>
                 {footerList.map((footerListArray, idx) => 
-                    <div key={idx} className='flex flex-col gap-6'>
+                    <div key={idx} className='grid grid-span-full gap-4 md:gap-6'>
                         {footerListArray.map(item => 
                             <div key={item.id}>
                                 <p className={`${item.id === 1 ? 'text-[#D9D9D9]' : 'text-[#8E8E8E]'}`}>
